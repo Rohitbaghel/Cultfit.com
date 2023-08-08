6274609d02c32109d69b7cfccb0c5d0770e3aafe
Manthan Gaikwad <manthan.gaikwad@indusnet.co.in>
2023-08-08 13:27:05 +0530

    change DB credential

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,9 @@
 const mongoose=require('mongoose');
 
+const MONGODB_USER=process.env.MONGODB_USER_ID
+const MONGODB_Pass=process.env.MONGODB_PASS
+const MONGODB_Cluster = process.env.MONGODB_CLUSTOR
 const connect =()=>{
-    return mongoose.connect('mongodb+srv://Manthan369:CCV0KBmkvKjSKPZI@cluster0.2dlpirc.mongodb.net/?retryWrites=true&w=majority');
+    return mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_Pass}@cluster0.${MONGODB_Cluster}.mongodb.net/test`);
 }
-
 module.exports =connect;
